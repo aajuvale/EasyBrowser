ed46185e93ded33be6b3a23338ed2e40b321c260
aajuvale <ahmed.juvale@gmail.com>
2025-08-21 14:03:09 -0700

    Initial Commit

---
+++ EasyBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyBrowser
+//
+//  Created by Ahmed Juvale on 8/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
